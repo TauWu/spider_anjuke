d22f9e34da6624d7f05455b2fd14937d2ff4d7ba
TauWoo <tauwoo@seuxw.cc>
2018-04-11 16:59:54 +0800

    正则存入常量模块

--- constant/value.py
+++ constant/value.py
@@ -1,6 +1,13 @@
+# 请求头常量
 headers = {
     "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36"
 }
 
+# 链接常量
 RAW_URL = "https://sh.zu.anjuke.com/v3/ajax/map/rent/3219/prop_list/?p={page}&maxp=99&room_num=0&price_id=0&rent_type=0&price_min=0&price_max=0&lx_id=&tag_id=&orient_id=&order_id=0&lat=31.064245_31.527833&lng=121.108875_121.833269&zoom=12&ib=1&et=d8eab6&bst=pzm190"
 AJKPageURL = "https://sh.zu.anjuke.com/fangyuan/{house_id}?from=Filter_1&hfilter=filterlist"
+
+# 正则匹配常量
+import re
+
+PAGE_ID_COMPLIE = re.compile(r"fangyuan/(.+)\?from=")
--- do/page_extractor.py
+++ do/page_extractor.py
@@ -4,6 +4,7 @@ from .house_selector import HouseSelector
 from module.downloader.page_extractor import PageExtratorDLD
 from module.spider.page_extractor import PageExtractorSPR
 from module.redis.page_extractor import PageExtractorRDS
+from constant.value import PAGE_ID_COMPLIE
 import re
 
 class PageExtractor():
@@ -18,11 +19,11 @@ class PageExtractor():
 
         for house_id_list in hs_iter:
             pe_downloader = PageExtratorDLD(house_id_list)
-            text_list = list()
             for rec in pe_downloader.res:
+                house_id = re.findall(PAGE_ID_COMPLIE,rec[1])[0]
                 if len(rec[0]) == 0:
                     '''详情页面在请求完成后没有返回数据 错误代码：1'''
-                    pe_rds.insert_empty_id(re.findall(r"fangyuan/(.+)\?from=",rec[1])[0])
+                    pe_rds.insert_empty_id(house_id)
                 else:
                     PageExtractorSPR(rec[0])
                     a = input("DEBUG")
