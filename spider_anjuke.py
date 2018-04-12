22fe77b1359ee679de2d9a1264302f4fdc27011a
TauWoo <tauwoo@seuxw.cc>
2018-04-12 13:39:58 +0800

    页面信息数据库操作

--- spider_anjuke.py
+++ spider_anjuke.py
@@ -4,4 +4,6 @@ from do.house_selector import HouseSelector
 from do.page_extractor import PageExtractor
 
 if __name__ == "__main__":
-    PageExtractor.first_page_extract()
+    # HouseSelector.spider_to_rds()
+    # HouseSelector.rds_to_db()
+    PageExtractor.first_page_extract(50)
